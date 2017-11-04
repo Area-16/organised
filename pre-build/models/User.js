523307a7dfa9240f3065282955e01d44963cd091
Douglas Alves <douglas.expalves@gmail.com>
2017-11-04 14:04:14 -0200

    updated session service and its model

--- pre-build/models/User.js
+++ pre-build/models/User.js
@@ -5,10 +5,20 @@ const Database = Db.default()
 const usuarios = Database.define('users', {
   id: {
     type: INTEGER,
-    primaryKey: true
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: STRING({ length: 150 }),
+    allowNull: false
   },
   usuario: {
-    type: STRING({ length: 30 })
+    type: STRING({ length: 30 }),
+    allowNull: false
+  },
+  senha: {
+    type: STRING,
+    allowNull: false
   }
 }, {
   timestamps: true,
--- pre-build/services/UsersService.js
+++ pre-build/services/UsersService.js
@@ -7,15 +7,26 @@ export class UserService {
     this.db = db
   }
 
+  createUser(userPayload) {
+    return new Promise((resolve, reject) => {
+      this.db.create(userPayload).then((response) =>{
+        resolve(response)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+    })
+  }
+
   deleteById(id) {
     return new Promise((resolve, reject) => {
       this.db
         .destroy({ id: id })
-        .then(() => {
+        .then((response) => {
           resolve('User deleted!')
         })
         .catch(err => {
-          reject(err.message)
+          reject(err)
         })
     })
   }
